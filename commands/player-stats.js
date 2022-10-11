048665f30091393ce80e9ec36ebec69b5ca52f52
trebla-88 <al.becquet8@gmail.com>
2022-10-11 21:56:12 +0200

    Amélioration commande stats

--- commands/player-stats.js
+++ commands/player-stats.js
@@ -1,10 +1,37 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { Sequelize, Op } = require('sequelize');
 const { dbRoute } = require('../config.json');
+const QuickChart = require('quickchart-js');
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('player-stats')
-		.setDescription('Montre les stats d\' un joueur!'),
+		.setDescription('Montre les stats d\' un joueur!')
+        .addStringOption(option =>
+            option.setName('player-tag')
+                .setDescription('Player Tag of the member.')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('category')
+                .setDescription('Stats category.')
+                .setRequired(true))
+                .addChoices(
+                    { name: 'Trophies', value: 'trophies' },
+                    { name: 'War Stars', value: 'stars' },
+                    { name: 'Donations', value: 'donations' },
+                    { name: 'Contributions', value: 'contributions' },
+                )
+        .addStringOption(option =>
+            option.setName('time')
+                .setDescription('Time range.')
+                .setRequired(true))
+                .addChoices(
+                    { name: 'Day', value: 'day' },
+                    { name: 'Week', value: 'week' },
+                    { name: 'Month', value: 'month' },
+                    { name: 'Year', value: 'year' },
+                    { name: 'All Time', value: 'all-time' },
+                ),
 	async execute(interaction) {
         // Connexion à la base de données
         const sequelize = new Sequelize('database', 'username', 'password', {
@@ -26,13 +53,57 @@ module.exports = {
             console.error('Unable to connect to the database:', error);
         }
 
+        // Récupération options
+        const player_tag = interaction.options.getString('player_tag');
+        const category = interaction.options.getString('category');
+        const time = interaction.options.getString('time');
+
+        // Choix des attributs
+        const atr = ['stats_date'];
+        switch (category) {
+            case 'trophies':
+                atr.push('trophies_count');
+                break;
+            case 'stars':
+                atr.push('star_count');
+                break;
+            case 'donations':
+                atr.push('donations_count');
+                break;
+            case 'contributions':
+                atr.push('contributions_count');
+                break;
+        }
+
+        let date = new Date();
+
+        // Choix de la date pour intervalle
+        switch (time) {
+            case 'day':
+                date = new Date(new Date() - 24 * 60 * 60 * 1000);
+                break;
+            case 'week':
+                date = new Date(new Date() - 7 * 24 * 60 * 60 * 1000);
+                break;
+            case 'month':
+                date = new Date(new Date() - 30 * 24 * 60 * 60 * 1000);
+                break;
+            case 'year':
+                date = new Date(new Date() - 365 * 24 * 60 * 60 * 1000);
+                break;
+            case 'all-time':
+                date = new Date(new Date() - 10 * 365 * 24 * 60 * 60 * 1000);
+                break;
+        }
+
         // Récupération données DB
         const query = await Stats.findAll({
-            attributes: ['stats_date', 'trophies_count'],
+            attributes: atr,
             where: {
-                player_id: '2PRJVLY29',
+                player_id: player_tag,
                 stats_date: {
-                    [Op.gt]: '2022-10-09 22:13:17',
+                    [Op.lt]: new Date(),
+                    [Op.gt]: date,
                 },
             },
             raw: true,
@@ -41,39 +112,105 @@ module.exports = {
         // Debugging purposes
         // console.log(query);
 
-        // Raccourcissemnt de la liste
-        const querySmall = query.slice(-10);
+        // Raccourcissement de la liste
+        let querySmall = [];
+        switch (time) {
+            case 'day':
+                querySmall = query;
+                break;
+            case 'week':
+                for (const elem of query) {
+                    if (querySmall != [] || querySmall[-1].stats_date.getDate != elem.stats_date.getDate) {
+                        querySmall.push(elem);
+                    }
+                }
+                break;
+            case 'month':
+                for (const elem of query) {
+                    if (querySmall != [] || querySmall[-1].stats_date.getDate != elem.stats_date.getDate) {
+                        querySmall.push(elem);
+                    }
+                }
+                break;
+            case 'year':
+                for (const elem of query) {
+                    if (querySmall != [] || querySmall[-1].stats_date.getMonth != elem.stats_date.getMonth) {
+                        querySmall.push(elem);
+                    }
+                }
+                break;
+            case 'all-time':
+                for (const elem of query) {
+                    if (querySmall != [] || querySmall[-1].stats_date.getFullYear != elem.stats_date.getFullYear) {
+                        querySmall.push(elem);
+                    }
+                }
+                break;
+        }
 
         // Définition des listes
         const dates = [];
-        const trophies = [];
+        const data = [];
 
-        for (const stat of querySmall) {
-            dates.push(stat.stats_date.slice(11, 19));
-            trophies.push(stat.trophies_count);
+        switch (category) {
+            case 'trophies':
+                for (const stat of querySmall) {
+                    dates.push(stat.stats_date);
+                    data.push(stat.trophies_count);
+                }
+                break;
+            case 'stars':
+                for (const stat of querySmall) {
+                    dates.push(stat.stats_date);
+                    data.push(stat.star_count);
+                }
+                break;
+            case 'donations':
+                for (const stat of querySmall) {
+                    dates.push(stat.stats_date);
+                    data.push(stat.donations_count);
+                }
+                break;
+            case 'contributions':
+                for (const stat of querySmall) {
+                    dates.push(stat.stats_date);
+                    data.push(stat.contributions_count);
+                }
+                break;
         }
 
         // Debugging purposes
         // console.log(dates, trophies);
 
-        const chart = {
+        const chart = new QuickChart();
+
+        chart.setVersion('3');
+
+        chart.setConfig({
             type: 'line',
             data: {
                 labels: dates,
                 datasets: [{
-                    label: 'Trophées',
-                    data: trophies,
+                    label: category,
+                    data: data,
+                    fill: false,
+                    tension: 0.3,
+                    borderColor: '#cc65fe',
                 }],
             },
-        };
-
-        const encodedChart = encodeURIComponent(JSON.stringify(chart));
-
-        const chartUrl = `https://quickchart.io/chart?c=${encodedChart}`;
+            options: {
+                scales: {
+                    y: {
+                        suggestedMin: Math.min(data),
+                        suggestedMax: Math.max(data),
+                    },
+                },
+            },
+        });
 
         // Debugging purposes
         // console.log(chartUrl);
 
-		return interaction.reply(chartUrl);
+		return interaction.reply(chart.getUrl());
 	},
 };
